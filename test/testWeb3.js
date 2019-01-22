139d2fb14e1ca2597eb7491e0b7900cb109dc636
HeeseokSon <hsson@syncodax.com>
2019-01-22 17:29:31 +0900

    TDD 코드 템플릿 추가

---
+++ test/testWeb3.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const assert = require('assert');
+
+suite('Web3', function(){
+    suite('Utils Test', function(){
+        setup(function(){
+        });
+        teardown(function(){
+        });
+        test('BN test', function(){
+        });
+    });
+});
