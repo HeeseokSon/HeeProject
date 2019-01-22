92b76616ac4b8fa39bb337383bbfeca51de5ceeb
HeeseokSon <hsson@syncodax.com>
2019-01-22 18:17:30 +0900

    TDD 코드 템플릿 추가2

--- test/testWeb3.js
+++ test/testWeb3.js
@@ -4,11 +4,14 @@ const assert = require('assert');
 
 suite('Web3', function(){
     suite('Utils Test', function(){
+        var Web3 = require('web3');
+        
         setup(function(){
         });
         teardown(function(){
         });
         test('BN test', function(){
+            assert.equal('1', '2', '3');
         });
     });
 });
